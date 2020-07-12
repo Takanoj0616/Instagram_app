a98ee7e152de0bb98e4b3d77494141c9de8437fa
vagrant <vagrant@localhost.localdomain>
2020-07-12 02:28:04 +0000

    app/views/posts/new.html.erb更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+   controllers: { registrations: 'registrations' }
   root 'pages#home'
   get '/users/:id', to: 'users#show', as: 'user'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end
 end
