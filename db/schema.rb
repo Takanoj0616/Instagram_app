b6f79953c6a1849fea0f682a0c69bab5f426e2d4
vagrant <vagrant@localhost.localdomain>
2020-07-28 02:29:44 +0000

    app/controllers/application_controller.rb更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_032955) do
+ActiveRecord::Schema.define(version: 2020_07_28_022252) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_07_16_032955) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "profile_photo"
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
